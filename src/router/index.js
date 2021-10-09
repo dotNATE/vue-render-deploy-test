7d1b602406ed44425664bf678b2d353a91cf56a3
dotNate <somersetnathaniel@gmail.com>
2021-10-09 16:27:39 +0100

    vuex deployment test

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EventList from '@/views/EventList.vue'
 import EventDetails from '@/views/EventDetails.vue'
+import EventCreate from '@/views/EventCreate.vue'
 import About from '@/views/About.vue'
 
 const routes = [
@@ -14,6 +15,12 @@ const routes = [
     name: 'EventDetails',
     props: true,
     component: EventDetails
+  },
+
+  {
+    path: '/event/create',
+    name: 'EventCreate',
+    component: EventCreate
   },
   {
     path: '/about',
